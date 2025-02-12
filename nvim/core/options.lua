4ca874fd08dc612b09a52dd82007c8398c245c42
Casey Turner <7030crt@gmail.com>
2025-02-12 15:46:49 -0700

    Update options.lua

--- nvim/core/options.lua
+++ nvim/core/options.lua
@@ -1,1 +1,17 @@
+-- options.lua
 
+-- Make line numbers default
+vim.opt.number = true
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Case-insensitive searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
