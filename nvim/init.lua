332af9d22dbc28e06d2f90c3d08872ce76633153
Casey Turner <7030crt@gmail.com>
2025-02-12 15:38:56 -0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,7 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
 
+require("core.options")
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
